c92feab2637fe1a5921b4ea757b2cba45d5bc518
Juciano Cardoso <cjuciano@gmail.com>
2017-09-16 04:36:31 -0300

    fixed database connection

--- project/config.py
+++ project/config.py
@@ -4,5 +4,5 @@ DEBUG = True
 SECRET_KEY = 'devgrid'
 CELERY_BROKER_URL = "amqp://localhost//"
 # CELERY_BACKEND = "db+sqlite:///{}/database.db".format(sys.path[0])
-SQLALCHEMY_DATABASE_URI = "sqlite://{}/database.db".format(sys.path[0])
+SQLALCHEMY_DATABASE_URI = "sqlite:////{}/database.db".format(sys.path[0])
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- project/views.py
+++ project/views.py
@@ -1,15 +1,15 @@
 from __future__ import absolute_import, unicode_literals
 from project.app import app, celery, db
-from flask import request
+from flask import request, jsonify
 
 @app.route('/receiver', methods=['POST'])
 def receiver():
-    event_string = request.data
-    save.delay(event_string, db)
+    event_string = str(request.data)
+    save.delay(event_string)
     return jsonify({'success': True}), 200
 
-@celery.task(name='project.parse')
-def save(event_string, db):
+@celery.task(name='project.save')
+def save(event_string):
     from project.event import EventFactory
     event = EventFactory.event_from_string(event_string)
     event.save(db)
